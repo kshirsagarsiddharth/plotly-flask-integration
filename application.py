685f6f0879e501e67703de0cce2fbd30a690cb52
Siddharth Kshirsagar <kshirsagarsiddharth45@gmail.com>
2022-02-21 22:08:36 +0530

    added requirements

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask_dir import app, flask_app, app2
 from flask_dir.plotlydash.dashboard import layout, layout2
 app.layout = layout
 app2.layout = layout2
-
+application = flask_app
 
 
 if __name__ == "__main__":
